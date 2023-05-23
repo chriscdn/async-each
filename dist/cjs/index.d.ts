7bb054e4bf1cc1f7af44cc4a1afd43e501964c19
Christopher Meyer <chris@schwiiz.org>
2023-05-23 09:41:01 +0200

    dist

---
+++ dist/cjs/index.d.ts
@@ -0,0 +1,12 @@
+export type StatusType<T, R> = {
+    progress: number;
+    total: number;
+    percent: number;
+    item: T;
+    index: number;
+    result: R;
+};
+type CallbackFn<T, R> = (item: T, index: number, items: Array<T>) => Promise<R>;
+type StatusCallbackFn<T, R> = (status: StatusType<T, R>) => void;
+declare function asyncEach<T, R>(items: Array<T>, callbackFn: CallbackFn<T, R>, statusCallbackFn?: StatusCallbackFn<T, R>): Promise<Array<R>>;
+export default asyncEach;
---
+++ dist/cjs/index.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function asyncEach(items, callbackFn, statusCallbackFn = () => { }) {
+    let progress = 0;
+    const total = items.length;
+    const promises = items.map((item, index, items) => {
+        return new Promise(async (resolve, reject) => {
+            setTimeout(async () => {
+                try {
+                    const result = await callbackFn(item, index, items);
+                    resolve(result);
+                    statusCallbackFn({
+                        progress: progress++,
+                        total,
+                        percent: Math.floor((100 * progress) / total),
+                        item,
+                        index,
+                        result,
+                    });
+                }
+                catch (err) {
+                    reject(err);
+                }
+            }, 0);
+        });
+    });
+    // This will reject if any promise fails.
+    return Promise.all(promises);
+}
+exports.default = asyncEach;
+//# sourceMappingURL=index.js.map
